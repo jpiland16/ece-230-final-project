094e8139d7c2bcc35bea9eb6df68b8e2bda4f871
Jonathan Piland <jonathan.piland@duke.edu>
2022-11-23 17:23:33 -0500

    light up segments D-H

--- main.asm
+++ main.asm
@@ -1,4 +1,4 @@
-    PROCESSOR	10F200
+    PROCESSOR    10F200
 #include <xc.inc>
     config WDTE  = OFF ; Disable watchdog timer
     config CP    = OFF ; Disable code protection
@@ -6,33 +6,54 @@
     PSECT resetVec,class=CODE,delta=2,abs
 resetVec:
 INIT:
-    movlw  ~(1 << 5)                  ; Enable GPIO2 by disabling T0CS = 5th bit
-    option                            ; and copy this value into OPTION register
-    movlw  11111000B                  ; turn on GPIO 0, 1, and 2
-    tris   GPIO                       ; Copy W into GPIO tristate register
+    movlw   ~(1 << 5)            ; Enable GPIO2 by disabling T0CS = 5th bit
+    option                ; and copy this value into OPTION register
+    movlw   11111000B            ; turn on GPIO 0, 1, and 2
+    tris    GPIO            ; Copy W into GPIO tristate register
 LOOP:
-    bsf    GPIO, GPIO_GP0_POSITION    ; Set GP0's bit to 1 (turn ON  GP0)
-    bsf    GPIO, GPIO_GP1_POSITION    ; Set GP1's bit to 1 (turn ON  GP1)
-    bcf    GPIO, GPIO_GP2_POSITION    ; Set GP2's bit to 0 (turn OFF GP2)
-    call   DELAY
-    call   DELAY
-    bcf    GPIO, GPIO_GP0_POSITION    ; Set GP0's bit to 0 (turn OFF GP0)
-    bcf    GPIO, GPIO_GP1_POSITION    ; Set GP1's bit to 0 (turn OFF GP1)
-    bsf    GPIO, GPIO_GP2_POSITION    ; Set GP2's bit to 1 (turn ON  GP2)
-    call   DELAY
-    call   DELAY
-    goto   LOOP
-DELAY:
-    movlw  0
-    movwf  0x10
-    movlw  200
-    movwf  0x11
-DELAY_LOOP:
-    decfsz 0x10, F
-    goto   DELAY_LOOP
-    decfsz 0x11, F
-    goto   DELAY_LOOP
-    retlw 0
+    ; DATA:  GP0
+    ; CLOCK: GP1
+    ; LATCH: GP2
+    bcf     GPIO, GPIO_GP2_POSITION     ; Clear GP2 (set LATCH to LOW) 
+    
+    bsf     GPIO, GPIO_GP0_POSITION     ; Set GP0    (DATA = 1)
+    
+                                        ;  -- PULSE CLOCK --
+    bsf     GPIO, GPIO_GP1_POSITION     ; Set GP1   (set CLOCK to HIGH)
+    bcf     GPIO, GPIO_GP1_POSITION     ; Clear GP1 (set CLOCK to LOW)
+    
+                                        ;  -- PULSE CLOCK -- 2
+    bsf     GPIO, GPIO_GP1_POSITION     ; Set GP1   (set CLOCK to HIGH)
+    bcf     GPIO, GPIO_GP1_POSITION     ; Clear GP1 (set CLOCK to LOW)
+    
+                                        ;  -- PULSE CLOCK -- 3
+    bsf     GPIO, GPIO_GP1_POSITION     ; Set GP1   (set CLOCK to HIGH)
+    bcf     GPIO, GPIO_GP1_POSITION     ; Clear GP1 (set CLOCK to LOW)
+    
+                                        ;  -- PULSE CLOCK -- 4
+    bsf     GPIO, GPIO_GP1_POSITION     ; Set GP1   (set CLOCK to HIGH)
+    bcf     GPIO, GPIO_GP1_POSITION     ; Clear GP1 (set CLOCK to LOW)
+    
+                                        ;  -- PULSE CLOCK -- 5
+    bsf     GPIO, GPIO_GP1_POSITION     ; Set GP1   (set CLOCK to HIGH)
+    bcf     GPIO, GPIO_GP1_POSITION     ; Clear GP1 (set CLOCK to LOW)
+
+    bcf     GPIO, GPIO_GP0_POSITION     ; Clear GP0 (DATA = 0)
+    
+                                        ;  -- PULSE CLOCK -- 6
+    bsf     GPIO, GPIO_GP1_POSITION     ; Set GP1   (set CLOCK to HIGH)
+    bcf     GPIO, GPIO_GP1_POSITION     ; Clear GP1 (set CLOCK to LOW)
+    
+                                        ;  -- PULSE CLOCK -- 7
+    bsf     GPIO, GPIO_GP1_POSITION     ; Set GP1   (set CLOCK to HIGH)
+    bcf     GPIO, GPIO_GP1_POSITION     ; Clear GP1 (set CLOCK to LOW)
+    
+                                        ;  -- PULSE CLOCK -- 8
+    bsf     GPIO, GPIO_GP1_POSITION     ; Set GP1   (set CLOCK to HIGH)
+    bcf     GPIO, GPIO_GP1_POSITION     ; Clear GP1 (set CLOCK to LOW)
+    
+    bsf     GPIO, GPIO_GP2_POSITION     ; Set GP2   (set LATCH to HIGH)
+    goto    LOOP
     
 END resetVec
 
